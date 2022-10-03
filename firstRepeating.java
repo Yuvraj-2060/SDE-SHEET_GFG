b8f435bc19bb675bc7c4d14cc12fa6f2d133c489
Mr . Singh <103349788+Yuvraj-2060@users.noreply.github.com>
2022-10-03 14:17:31 +0530

    Create firstRepeating.java
    Input: ch = “geeksforgeeks” 
    Output: e 
    e is the first element that repeats
    
    Input: str = “hello geeks” 
    Output: l 
    l is the first element that repeats

---
+++ firstRepeating.java
@@ -0,0 +1,34 @@
+// Java program to find the first
+// repeated character in a string
+import java.util.*;
+
+class Main
+{
+	
+	static char firstRepeating(char str[])
+	{
+		
+		HashSet<Character> h = new HashSet<>();
+
+		// Traverse the input array from left to right
+		for (int i=0; i<=str.length-1; i++)
+		{
+			char c = str[i];
+			if (h.contains(c))
+				return c;
+
+			else 
+				h.add(c);
+		}
+
+		return '\0';
+	}
+
+	// Driver method to test above method
+	public static void main (String[] args)
+	{
+		String str = "geeksforgeeks";
+		char[] arr = str.toCharArray();
+		System.out.println(firstRepeating(arr));
+	}
+}
